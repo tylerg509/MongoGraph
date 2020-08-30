a0a2cee924d9abadd1c7c24a308e3c5e2b2f0084
Tyler Gasperlin <gasperlint@gmail.com>
2020-08-30 12:15:27 -0700

    Get data via graphql mongo

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import {graphql} from 'react-apollo';
 
 class SongList extends Component {
     render() {
+        console.log(this.props)
         return(
             <div>Songlist</div>
 
@@ -9,4 +12,14 @@ class SongList extends Component {
     }
 }
 
-export default SongList;
+// GraphQl Queries are not valid JS so we use graphql tag
+const query = gql`
+    {
+        songs {
+            title
+        }
+    }
+`;
+
+// graphql(query) executes the query. this bonds the query to the component
+export default graphql(query)(SongList);
