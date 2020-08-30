f0d8123aac5eacc02b54a7e183df4963a8a2b3b4
Tyler Gasperlin <gasperlint@gmail.com>
2020-08-30 12:07:14 -0700

    Add songlist component

---
+++ client/components/SongList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class SongList extends Component {
+    render() {
+        return(
+            <div>Songlist</div>
+
+        )
+    }
+}
+
+export default SongList;
