23427cf351b798c6d9a4c38629266cb8be2908ec
Tyler Gasperlin <gasperlint@gmail.com>
2020-09-01 06:31:43 -0700

    Add react router and a form

--- client/index.js
+++ client/index.js
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom';
 import AppolloClient, { ApolloClient } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import SongList from './components/SongList';
-
+import { Router, Route, hashHistory, IndexRoute } from 'react-router'
 const client = new ApolloClient({})
+import App from './components/App'
+import SongCreate from './components/SongCreate'
 
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <SongList/>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList}/>
+          <Route path="song/new" component={SongCreate}/> 
+        </Route>
+      </Router>
     </ApolloProvider>
   )
 };
