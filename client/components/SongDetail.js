8c64448c9dff5eaf91945842b78bbf260a8fbe68
Tyler Gasperlin <gasperlint@gmail.com>
2020-09-08 12:13:39 -0700

    Display song name in detail page

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo'
+import fetchSong from '../queries/fetchSong'
 
 class SongDetail extends Component {
+
     render() {
+        const { song } = this.props.data
+
+        if (!song) { return <div> Loading...</div>}
+
         return (
             <div>
                 <h3>
-                    Song  Detail
+                    {song.title}
                 </h3>
             </div>
         )
@@ -14,4 +21,6 @@ class SongDetail extends Component {
 
 
 
-export default SongDetail;
+export default graphql(fetchSong, {
+    options: (props) => { return { variables: { id: props.params.id} } }
+})(SongDetail);
